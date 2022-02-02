6aafddcc661d8255b1dcff74c6a3e194a6688d90
Philipp <philipp.stangl95@gmail.com>
2022-02-02 14:41:04 +0100

    Move OCW listener

--- sys-src/offchain/ocw.py
+++ sys-src/offchain/ocw.py
@@ -1,8 +1,12 @@
+"""
+OCW Listener
+Listens for off-chain worker notifications on port 3005.
+"""
+
 from flask import Flask, request, jsonify
 from flask_cors import cross_origin
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['POST'])
 @cross_origin()
@@ -14,4 +18,5 @@ def main():
 
 
 if __name__ == '__main__':
+    # When changing the port, don't forget to change the port in the product-tracking pallet.
     app.run(host="localhost", port=3005, debug=True)
